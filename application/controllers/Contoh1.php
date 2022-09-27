4501221893eded73472685996412433e8357f29a
mfimbt07 <fairus.serpong01@gmail.com>
2022-09-27 13:59:01 +0700

    tugas pertemuan 3

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Hallo</h1>";
+		echo"Nama Saya Fairus 
+			Saya tinggal di daerah Kademangan
+			Olahraga yang saya sukai adalah
+			Sepak Bola";
+	}
+}
